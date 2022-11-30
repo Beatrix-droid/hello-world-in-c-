a583ac262c8e33ab43822846e754392fe8244b8f
Beatrice <beatrice.federici123@gmail.com>
2022-11-30 17:26:28 +0000

    added notes on while and do while loops

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-          Console.WriteLine(GetDay(5));
+        int index=1;
+        while (index <= 5){
+            Console.WriteLine(index);
+            index++;
+        }
+
+        int index_2 = 6;
+        //this is a do while loop, first executes code, and then checks the condition. This loop will print out 6 and then stop
+
+        do{
+            Console.WriteLine(index_2);
+            index_2 ++;
+        } while (index_2 <=5);
         }
 
         // we can also have methods that return things. in which case we will not put "void" next to statci but the data type we want to return. here in the cubing function we are returning an int, so static int
