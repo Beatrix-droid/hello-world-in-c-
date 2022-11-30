c08b420e2bfa2cc30fa36a0dace76f9df889d2b9
Beatrice <beatrice.federici123@gmail.com>
2022-11-30 17:03:39 +0000

    built simple calculator

--- Program.cs
+++ Program.cs
@@ -6,46 +6,52 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(CubeIt(8)); //call functions you created into main mehtod(or main function to execute them)
-            int cubed= CubeIt(3);
-            Console.WriteLine(cubed);
-        
-        //if statements
-            bool isFemale=false;
-            bool isTall=true;
-            //general structure of an if statement  if (condition){code for that condition}
-            if (isFemale && isTall){ // and is represented with two ampersands &&
-                Console.WriteLine("is a female and tall");
-            }   
-            else{
-                Console.WriteLine("You are not tall or female");
-            }     
-            if (isFemale || isTall){ //or is represented by ||
-                Console.WriteLine("You are either female or tall.");
-            }
-            //negation operator represente by a ! that you place in front of the variable name
+            string num_1;
+            string op;
+            string num_2;
 
-            Console.WriteLine(Maxit(3,7,3));
+            Console.WriteLine("Welcome to the simple calculator");
+            Console.WriteLine("Please the the first number");
+            
+            num_1=Console.ReadLine();
+            Console.WriteLine("Please the operator");
+            op=Console.ReadLine();
+            Console.WriteLine("Please enter the second number");
+            num_2= Console.ReadLine();
+            Console.WriteLine(Calculate(num_1,op, num_2));
         }
 
         // we can also have methods that return things. in which case we will not put "void" next to statci but the data type we want to return. here in the cubing function we are returning an int, so static int
+        static string Calculate(string num_1, string op, string num_2){
 
-        static int  CubeIt(int number){    // this method will not return any info, but jst print to the console (methods are functions in c#)
-            int result = number*number*number;
-            return result;
-        }
-        
-        static string Maxit(int n1, int n2, int n3){
+            string result;
 
-               string T;
-                if (n1 >= n2 && n1==n3){
-                    T= n1.ToString() + n3.ToString() + " are equal";
-                }
-                else{
-                    T=n2.ToString();
-                }
-            return T.ToString();
 
+            try{
+                Convert.ToDecimal(num_1);
+                Convert.ToDecimal(num_2);
+            }
+            catch(System.FormatException ex){
+                Console.WriteLine("operands incorrecty entered. Please make sure to enter numbers only");
+            }
+
+            if (op =="+"){
+                result=(Convert.ToDecimal(num_1) + Convert.ToDecimal(num_2)).ToString();
+            }
+            else if( op == "-"){
+                 result=(Convert.ToDecimal(num_1) - Convert.ToDecimal(num_2)).ToString();
+            }
+            else if (op == "/"){
+                 result=(Convert.ToDecimal(num_1)/Convert.ToDecimal(num_2)).ToString();
+            }
+            else if (op =="*"){
+                 result=(Convert.ToDecimal(num_1) * Convert.ToDecimal(num_2)).ToString();      
+            }
+
+            else{
+                result = "operators or numbers not valid. Please reenter any of the accepted ones: *, -, +, /";
+            }
+            return result.ToString();
         }
     }
 }
